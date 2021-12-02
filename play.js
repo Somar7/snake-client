509fedf9e2b3cb8523129496768cf6904965b6d7
somar7 <somar7@nait.ca>
2021-12-02 00:23:05 +0000

    Pulling

--- play.js
+++ play.js
@@ -1,30 +1,4 @@
-const net = require("net");
-let IP = '165.227.47.243';
-const PORT = 50541;
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: IP, // IP address here,
-    port: PORT // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log("Successfully connected to game server");
-    conn.write("Name: Slz")
-
-    // conn.write("Move: up")
-    // setTimeout(() => {
-    //     conn.write("Move: up"); 
-    // }, 300)
-})
-// interpret incoming data as text
-conn.on("data", (data) => {
-    console.log(data)
-})
-  return conn;
-};
+const { connect } = require('./client');
 
 console.log("Connecting ...");
 connect();
